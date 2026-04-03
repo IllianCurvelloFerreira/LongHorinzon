45a69e7a46cd372d419c5e1c821007360d9e2b30
IllianCurvelloFerreira <illiancurve@gmail.com>
2026-04-03 17:07:50 -0300

    data fix

--- datasets/ett_gbt.py
+++ datasets/ett_gbt.py
@@ -28,19 +28,19 @@ def ensure_ett_csv(root_path: str | Path, data_name: str, cache_dir="./nixtla_ca
     preprocess_fn = None
 
     try:
-        from .preprocess_ett import preprocess_ett_dataset
+        from ..data_loader.preprocess_ett import preprocess_ett_dataset
         preprocess_fn = preprocess_ett_dataset
     except ImportError:
         try:
-            from datasets.preprocess_ett import preprocess_ett_dataset
+            from data_loader.preprocess_ett import preprocess_ett_dataset
             preprocess_fn = preprocess_ett_dataset
         except ImportError:
             try:
-                from .preprocess_ett import preprocess_ett_dataset
+                from ..data_loader.preprocess_ett import preprocess_ett_dataset
                 preprocess_fn = preprocess_ett_dataset
             except ImportError:
                 try:
-                    from datasets.preprocess_ett import preprocess_ett_dataset
+                    from data_loader.preprocess_ett import preprocess_ett_dataset
                     preprocess_fn = preprocess_ett_dataset
                 except ImportError:
                     pass
